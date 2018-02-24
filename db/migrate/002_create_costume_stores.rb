b9286a7e1fb7095f16ce9a4925981fd4d1fe1ec0
vsparrow <khan@vsparrow.com>
2018-02-24 13:41:13 -0500

    edit db/migration files

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+class ConstumeStore < ActiveRecord::Migration
+  def change
+    create_table costume_stores do |t|
+    t.string  :name
+    t.string  :location
+    t.integer :number_of_costumes
+    t.integer :number_of_employees
+    t.boolean :in_business
+    t.string  :opening_time
+    t.string  :closing_time
+    end
+  end #
+end #class
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+class HauntedHouse < ActiveRecord::Migration
+  def change
+    create_table haunted_houses do |t|
+          t.string :name
+          t.string :location
+          t.string :theme
+          t.integer :price
+          t.boolean :family_friendly
+          t.datetime :opening_date
+          t.datetime :closing_date
+          t.string :long_description
+    end
+  end
+end
